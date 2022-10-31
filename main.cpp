efbef172c6e2cd0021ad165b3388a1ffa2ffa3b5
zmmyslony <zmmyslony@gmail.com>
2022-10-31 15:55:51 +0000

    Moved tuning functions outside of a pattern loop so that they are executed only once.

--- main.cpp
+++ main.cpp
@@ -22,13 +22,12 @@ int main() {
                                                      "symmetric gauss curved rectangle",
                                                      "linear, 1x0.5 cm",
                                                      "azimuthal, r = 1 cm"};
-    patterns_to_generate = {"azimuthal, r = 1 cm"};
-//    patterns_to_generate = {"linear, 2x1 cm"};
+    patterns_to_generate = {"radial, r = 1 cm", "azimuthal, r = 1 cm"};
 
     // All units are in mm
     double cleaning_distance = 20; // Also allows the material to start flowing until we are in the shear thinning regime
 
-    ExtrusionConfiguration extrusion_configuration(50, 20, 0.2, 0.08, 1);
+    ExtrusionConfiguration extrusion_configuration(75, 20, 0.2, 0.08, 1);
     PrinterConfiguration printing_configuration(1000, 0, 2);
 
     int uv_duty_cycle = 30;
@@ -42,20 +41,25 @@ int main() {
 
     std::cout << "\nGenerating GCode for the files contained in" << std::endl << '\t' << patterns_directory
               << std::endl;
+
     for (auto &pattern: patterns_to_generate) {
         singleLayer(patterns_directory, pattern, grid_spacing, pattern_offset, cleaning_distance,
                     tool_offset, uv_duty_cycle, first_layer_height, extrusion_configuration,
                     printing_configuration);
+
         multiLayer(patterns_directory, pattern, grid_spacing, pattern_offset, cleaning_distance,
                    tool_offset, uv_duty_cycle, first_layer_height, 4, extrusion_configuration,
                    printing_configuration);
-        tuneLineSeparation(patterns_directory, 10, tool_offset, uv_duty_cycle, first_layer_height,
-                           extrusion_configuration, printing_configuration, 2, 0.75, 0.25);
-        tuneLineSeparationAndHeight(patterns_directory, 10, tool_offset, uv_duty_cycle, first_layer_height,
-                                    extrusion_configuration, printing_configuration,
-                                    1.5, 1, 0.25,
-                                    0.09, 0.06, 0.01);
     }
+
+    tuneLineSeparation(patterns_directory, 10, tool_offset, uv_duty_cycle, first_layer_height,
+                       extrusion_configuration, printing_configuration,
+                       2, 0.75, 0.25);
+
+    tuneLineSeparationAndHeight(patterns_directory, 10, tool_offset, uv_duty_cycle, first_layer_height,
+                                extrusion_configuration, printing_configuration,
+                                1.5, 1, 0.25,
+                                0.09, 0.06, 0.01);
 
     std::cout << "Generation complete." << std::endl;
     return 0;
