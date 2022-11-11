e61ec41b040841301f636f5b50e75796d4816e63
zmmyslony <zmmyslony@gmail.com>
2022-11-11 15:55:38 +0000

    Updated copyright notice

--- source/engine/pattern_boundaries.h
+++ source/engine/pattern_boundaries.h
@@ -1,9 +1,15 @@
 /*
- * Copyright (c) 2022, Michał Zmyślony, mlz22@cam.ac.uk. All rights reserved.
+ * 2022, Michał Zmyślony, mlz22@cam.ac.uk.
  *
  * Please cite Michał Zmyślony and Dr John Biggins if you use any part of this code in work you publish or distribute.
  *
- * This file is a part of gCode Generator.
+ * This file is part of gCodeGenerator.
+ *
+ * gCodeGenerator is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+ *
+ * gCodeGenerator is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with Foobar. If not, see <https://www.gnu.org/licenses/>.
  */
 
 //
